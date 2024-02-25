6d8b6fcbef996fd0e9fb13eadd8e474556e090dd
abbeymaj <abbey.majumdar@gmail.com>
2024-02-25 17:38:43 -0500

    updated config_entity.py

--- src/components/config_entity.py
+++ src/components/config_entity.py
@@ -1,15 +1,22 @@
 # Importing packages
 import os
-import sys
 from dataclasses import dataclass
 
-# Creating a class for the Data Ingestion Configuration
+# Creating a class for the Data Ingestion process.
 
 @dataclass
 class DataIngestionConfig():
     '''
-    This class will define the paths for the train data, test data and the raw data.
+    This class will define the paths for the train data and test data.
     '''
     train_data_path:str = os.path.join('artifacts', 'train.zip')
     test_data_path:str = os.path.join('artifacts', 'test.zip')
-    #raw_data_path:str = os.path.join('artifacts', 'data.csv')
+
+
+# Creating a class for the Data Transformation process.
+@dataclass
+class DataTransformationConfig():
+    '''
+    This class defines the path to the preprocessor object.
+    '''
+    preprocessor_obj_path:str = os.path.join('artifacts', 'preprocessor.pkl')
